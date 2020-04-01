3cc0e7a8236733b475d6264732583c7b047a3612
Nicola Bello <nicola.bello.3@gmail.com>
2020-04-01 12:38:25 +0100

    updated build

--- build.js
+++ build.js
@@ -1,20 +1,27 @@
-const fs = require("fs-extra");
+const rimraf = require('rimraf');
+const fs = require('fs-extra');
+const replace = require('replace-in-file');
 
-fs.copySync("node_modules/@material", "build/components");
-fs.copySync("node_modules/material-components-web", "build");
+const componentsFolder = 'components';
+const mainFolder = 'main';
 
-const replace = require("replace-in-file");
+rimraf.sync(componentsFolder);
+rimraf.sync(mainFolder);
+
+fs.copySync('node_modules/@material', componentsFolder);
+fs.copySync('node_modules/material-components-web', mainFolder);
+
 
 for (let i = 1; i < 10; i++) {
-  replace.sync({
-    files: `build/components/${new Array(i).fill("*").join("/")}/*.scss`,
-    from: /@material\//g,
-    to: `./${new Array(i).fill("..").join("/")}/`
-  });
+    replace.sync({
+        files: `${componentsFolder}/${new Array(i).fill('*').join('/')}/*.scss`,
+        from: /@material\//g,
+        to: `./${new Array(i).fill('..').join('/')}/`
+    });
 }
 
-  replace.sync({
-    files: `build/*.scss`,
+replace.sync({
+    files: `${mainFolder}/*.scss`,
     from: /@material\//g,
-    to: `./components/`
-  });
+    to: `./../${componentsFolder}/`
+});
