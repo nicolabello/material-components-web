278b69d1a17cd0a02ff2fefe199ae68e2e0bd1f4
Nicola Bello <nicola.bello.3@gmail.com>
2020-04-01 15:26:09 +0100

    updated build

--- build.js
+++ build.js
@@ -11,17 +11,26 @@ rimraf.sync(mainFolder);
 fs.copySync('node_modules/@material', componentsFolder);
 fs.copySync('node_modules/material-components-web', mainFolder);
 
+/*replace.sync({
+    files: [`${componentsFolder}/!**!/!*.ts`, `${mainFolder}/!**!/!*.ts`],
+    from: /module ("|')@material\//g,
+    to: `module $1material-components-web/`
+});*/
 
-for (let i = 1; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
+
+    let foldersPath = new Array(i).fill('*').join('/');
+    foldersPath = foldersPath ? `/${foldersPath}` : '';
+
+    let relativePath = new Array(i).fill('..').join('/');
+    relativePath = relativePath ? `/${relativePath}` : '';
+
+    const extensions = '{scss,js}';
+
     replace.sync({
-        files: `${componentsFolder}/${new Array(i).fill('*').join('/')}/*.scss`,
-        from: /@material\//g,
-        to: `./${new Array(i).fill('..').join('/')}/`
+        files: [`${componentsFolder}${foldersPath}/*.${extensions}`, `${mainFolder}${foldersPath}/*.${extensions}`],
+        from: /("|')@material\//g,
+        to: `$1.${relativePath}/`
     });
-}
 
-replace.sync({
-    files: `${mainFolder}/*.scss`,
-    from: /@material\//g,
-    to: `./../${componentsFolder}/`
-});
+}
